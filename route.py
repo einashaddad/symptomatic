147e90fa2aa94a9d1451919a6c5d05dba3f236ff
Einas Haddad <einas@haddad>
2013-04-04 15:29:38 -0400

    Fix bug with port

--- route.py
+++ route.py
@@ -110,5 +110,5 @@ def show_symptoms():
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT'), 58733)
-	app.run(debug=True, host='0.0.0.0'), port=port)
+	port = int(os.environ.get('PORT', 58733))
+	app.run(debug=True, host='0.0.0.0', port=port)
