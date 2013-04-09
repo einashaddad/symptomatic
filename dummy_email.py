cc467f6800a36e9cf685f572d9e24ee29d2d8dc1
Einas Haddad <einas@haddad>
2013-04-08 23:10:41 -0400

    Add models file

--- dummy_email.py
+++ dummy_email.py
@@ -9,11 +9,11 @@ This sends a dummy email to the app
 """
 
 data = {'sender': 'einas.haddad@gmail.com', 
-		'recipient': 'symtomatic@symtomatic.mailgun.org',
-		'subject': 'test',
-		'body-plain': 'craycray\nsymptom',
-		'timestamp': str(int(time.time())),		
-		}
+        'recipient': 'symtomatic@symtomatic.mailgun.org',
+        'subject': 'test',
+        'body-plain': 'craycray\nsymptom',
+        'timestamp': str(int(time.time())),     
+        }
 
 #r = requests.post('http://shrouded-tundra-4968.herokuapp.com/messages', data=data)
 r = requests.post('http://localhost:58733/messages', data=data)
---
+++ models.py
@@ -0,0 +1,67 @@
+import re
+import datetime
+
+class User(object):
+    def __init__(self, first_name, last_name, fb_email, email, birthday):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.fb_email = fb_email
+        self.email = email
+        self.birthday = birthday    
+
+    def to_json(self):
+        return  { "first_name": self.first_name, 
+                  "last_name":  self.last_name, 
+                  "fb_email":   self.fb_email, 
+                  "email" :     self.email, 
+                  "birthday" :  self.birthday,
+                }
+
+    @classmethod
+    def validate(cls, first_name, last_name, fb_email, email, birthday):
+
+        # TODO: Figure out more exceptions
+        if type(first_name) != unicode:
+            raise UserValidationError("first name is messed up")
+        elif type(last_name) != unicode:
+            raise UserValidationError("last name is bad")
+
+        return cls(first_name, last_name, fb_email, email, birthday)
+
+class Email(object):
+    def __init__(self, date, sender, symptoms, body_plain):
+        self.date = date
+        self.sender = sender
+        self.body_plain=body_plain
+        self.symptoms = symptoms
+
+    def to_json(self):
+        return {"date": self.date,
+                "sender": self.sender, 
+                "symptoms": self.symptoms, 
+                "body_plain": self.body_plain,
+                }
+
+    @classmethod
+    def validate(cls, date, sender, symptoms, body_plain):
+        # TODO: Figure out more exceptions
+
+        if type(date) != datetime.datetime:
+            raise EmailValidationError("date is not of type datetime")
+        elif body_plain.splitlines() != symptoms:
+            raise EmailValidationError("list of symptoms not as in email sent")
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", sender): 
+            raise EmailValidationError("invalid email address")
+
+        return cls(date=date, sender=sender, symptoms=symptoms, body_plain=body_plain)
+
+
+class EmailValidationError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+class UserValidationError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+
