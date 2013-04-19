f42d946277e2b617f2af8925d0eb2f6839d9227d
Einas Haddad <einas@haddad>
2013-04-19 15:29:54 -0400

    Add bare minimum bootstrap stylesheet

--- route.py
+++ route.py
@@ -214,4 +214,4 @@ def _verify(api_key, token, timestamp, signature):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 58733))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')#, port=port)
