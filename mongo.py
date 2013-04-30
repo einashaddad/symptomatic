2c45b28a18571d8bf137ed75b2271fb03f1b2c13
Allie Jones <allie.jones@gmail.com>
2013-04-30 16:36:08 -0400

    Add route for symptom counts in JSON format

--- mongo.py
+++ mongo.py
@@ -19,19 +19,19 @@ def save_email(e):
     Saves the symptoms along with the timestamp, sender and email in mongodb
     """
     email = db.email
-    email.insert(e.to_json()) 
+    email.insert(e.to_json())
 
 def find_symptoms(sender=None, start_date=None, end_date=None):
     """
     Returns the symptoms from mongodb
 
-    Finds the entry according to the sender (the person logged in), 
+    Finds the entry according to the sender (the person logged in),
     the start date and the end date
     """
     end_date += datetime.timedelta(1)
-    email = db.email 
-    messages = email.find({ "sender" : sender, 
-                            "date" : { "$gte" : start_date, 
+    email = db.email
+    messages = email.find({ "sender" : sender,
+                            "date" : { "$gte" : start_date,
                                        "$lt" : end_date } })
     result = Counter(symptom for message in messages for symptom in message['symptoms'])
     return [(str(s), result[s]*100/sum(result.values())) for s in result]
@@ -40,12 +40,35 @@ def find_all_symptoms(sender=None):
     """
     Returns all symptoms from mongodb
     """
-    email = db.email 
+    email = db.email
     messages = email.find({"sender" : sender})
     result = Counter(symptom for message in messages for symptom in message['symptoms'])
     most_common = result.most_common(10)
 
     return [(str(s[0]), result[s[0]]*100/sum(result.values())) for s in most_common]
+
+def find_symptoms_count(filter={}):
+    """
+    Returns the symptoms from mongodb as a raw count, rather than a percentage
+
+    Finds the entry according to the sender (the person logged in),
+    the start date and the end date
+    """
+    query = { 'sender': filter['sender'] }
+
+    if 'start_date' in filter:
+        if 'date' not in query: query['date'] = {}
+        query['date']['$gte'] = filter['start_date']
+
+    if 'end_date' in filter:
+        if 'date' not in query: query['date'] = {}
+        filter['end_date'] += datetime.timedelta(1)
+        query['date']['$lte'] = filter['end_date']
+
+    messages = db.email.find(query)
+    result = Counter(symptom for message in messages for symptom in message['symptoms'])
+    return [(str(s), result[s]) for s in result]
+
 
 def check_user(fb_email):
     """
@@ -56,12 +79,12 @@ def check_user(fb_email):
     user_found = user.find_one( { "fb_email": fb_email } )
 
     if user_found:
-        return user_found['email']  
-    return None     
+        return user_found['email']
+    return None
 
 def add_user(u):
     """
-    Accepts models.User class Adds a new user in mongodb 
+    Accepts models.User class Adds a new user in mongodb
     """
     user = db.user
     user.insert(u.to_json())
@@ -80,4 +103,4 @@ def verified(email):
 def delete_verified(email):
     not_verified = db.not_verified
     db.not_verified.remove({ "email": email })
-    
+
